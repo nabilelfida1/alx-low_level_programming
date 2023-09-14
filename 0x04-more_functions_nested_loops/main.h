9e6b19a3338e7f07fda72140f0fe5e2ddbd65b41
nabilelfida1 <mrnabilelfida@gmail.com>
2023-09-14 05:16:39 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,18 +1,21 @@
-#include "holberton.h"
-#include <stdio.h>
-
+#ifndef MAIN_H
+#define MAIN_H
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-  char c;
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+*/
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 
-  c = 'A';
-  printf("%c: %d\n", c, _isupper(c));
-  c = 'a';
-  printf("%c: %d\n", c, _isupper(c));
-  return (0);
-}
+#endif /* MAIN_H */
