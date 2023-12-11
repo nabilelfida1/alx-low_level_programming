b63701a3cc38c7d79f0ffc7a06dad0a72530f8ce
Nabilelfida1 <Mrnabilelfida@gmai.com>
2023-12-11 14:04:11 -0800

    task1

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,22 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+int mul(int a, int b) {
+    return a * b;
+}
+
+int div_func(int a, int b) {
+    return a / b;
+}
+
+int mod(int a, int b) {
+    return a % b;
+}
